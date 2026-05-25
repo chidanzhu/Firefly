5415e396b84d7ed2f8f84addd8cce4d87fd6cd45
chidanzhu <3490467566@qq.com>
2026-05-25 18:45:36 +0800

    更新了站点配置

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -9,10 +9,10 @@ export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.avif",
 
 	// 名字
-	name: "Firefly",
+	name: "丹朱",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "你好，我是丹朱",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
@@ -30,13 +30,13 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "GitHub",
 			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
+			url: "https://github.com/chidanzhu",
 			showName: false,
 		},
 		{
 			name: "Email",
 			icon: "fa7-solid:envelope",
-			url: "mailto:xiaye@msn.com",
+			url: "mailto:3490467566@qq.com",
 			showName: false,
 		},
 		{
